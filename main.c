b4516b1d87e460126ad0cf02de297468e8c7a7c3
Felix <felix.ulmanu@gmail.com>
2021-12-02 14:17:09 -0500

    More work on arguments (not working)

--- main.c
+++ main.c
@@ -5,37 +5,49 @@
 /* for command line argument parsing */
 #include <getopt.h>
 
-
 int main(int argc, char **argv) {
 
-   /* command line argument parsing */
-   int argIndex = 0;
+  /* command line argument parsing */
+  int argIndex = 0;
+  char *inputFile = NULL;
+  char *outputFile = NULL;
 
-   char *inputFile = NULL;
-   char *outputFile = NULL;
+  /* return if incorrect usage */
+  int badInput() {
+    printf("Incorrect usage \n");
+    return 1;
+  }
 
-   while ((argIndex = getopt(argc, argv, "i:o")) != -1 ) {
-      switch (argIndex) {
-         case 'i':
-            inputFile = optarg;
-            printf("Input argument detected \n");
-         break; case 'o':
-            outputFile = optarg;
-            printf("Output file detected \n");
-         break; default:
-            printf("Option error \n");
-            return 1;
-      }
-   }
+  while ((argIndex = getopt(argc, argv, "i:o")) != -1) {
+    switch (argIndex) {
+    case 'i':
+      inputFile = optarg + 1;
+      printf("inputFile is %s\n", inputFile);
+      break;
+    case 'o':
+      outputFile = optarg + 1;
+      printf("outputFile is %s\n", outputFile);
+      break;
+    default:
+      return badInput();
+    }
+  }
 
-   /* MagickWand *mw = NULL; */
-   /* mw = NewMagickWand(); */
+  /* fail with wrong usage message if mandatory options missing */
+  if (inputFile == NULL || outputFile == NULL) {
+    return badInput();
+  }
 
-   /* MagickReadImage(mw,"testimage.jpg"); */
-   /* MagickWriteImage(mw,"newtestimage.jpg"); */
+  /* initialise MagickWand */
+  MagickWand *mw = NULL;
+  mw = NewMagickWand();
 
-   /* printf("Done\n"); */
+  MagickReadImage(mw, inputFile);
+  MagickWriteImage(mw, outputFile);
 
-   /* mw = DestroyMagickWand(mw); */
-   return 0;
+  printf("Done\n");
+
+  mw = DestroyMagickWand(mw);
+
+  return 0;
 }
