2f6c48b6456e79f299b9a2ed044f0949bbc37b75
Felix <felix.ulmanu@gmail.com>
2021-12-02 12:08:19 -0500

    Initial argument parsing

--- main.c
+++ main.c
@@ -1,15 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+/* for imagemagick */
 #include <ImageMagick-7/MagickWand/MagickWand.h>
+/* for command line argument parsing */
+#include <getopt.h>
 
-int main() {
 
-   MagickWand *mv = NULL;
-   mv = NewMagickWand();
-   MagickReadImage(mv,"testimage.jpg");
-   MagickWriteImage(mv,"newtestimage.jpg");
+int main(int argc, char **argv) {
 
-   printf("Done\n");
+   /* command line argument parsing */
+   int argIndex = 0;
 
+   char *inputFile = NULL;
+   char *outputFile = NULL;
+
+   while ((argIndex = getopt(argc, argv, "i:o")) != -1 ) {
+      switch (argIndex) {
+         case 'i':
+            inputFile = optarg;
+            printf("Input argument detected \n");
+         break; case 'o':
+            outputFile = optarg;
+            printf("Output file detected \n");
+         break; default:
+            printf("Option error \n");
+            return 1;
+      }
+   }
+
+   /* MagickWand *mw = NULL; */
+   /* mw = NewMagickWand(); */
+
+   /* MagickReadImage(mw,"testimage.jpg"); */
+   /* MagickWriteImage(mw,"newtestimage.jpg"); */
+
+   /* printf("Done\n"); */
+
+   /* mw = DestroyMagickWand(mw); */
    return 0;
 }
