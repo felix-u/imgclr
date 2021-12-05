574c07abcee58c6ecaa8038d84bc06abc66000ed
Felix <felix.ulmanu@gmail.com>
2021-12-04 22:21:09 -0500

    Move to PPM and change sample images

--- main.c
+++ main.c
@@ -5,74 +5,76 @@
 /* for command line argument parsing and a couple other things*/
 #include <unistd.h>
 
+int badInput();
+
 int main(int argc, char **argv) {
 
-  /* command line argument parsing */
-  int argIndex = 0;
-  char *inputFile = NULL;
-  char *outputFile = NULL;
+    /* command line argument parsing */
+    int argIndex = 0;
+    char *inputFile = NULL;
+    char *outputFile = NULL;
 
-  /* return if incorrect usage */
-  int badInput(char *errorType) {
+    /* /\* begin implementing options - disabled for now *\/ */
+    /* while ((argIndex = getopt(argc, argv, "i:o")) != -1) { */
+    /*   switch (argIndex) { */
+    /*   case 'i': */
+    /*     inputFile = optarg + 1; */
+    /*     printf("inputFile is %s\n", inputFile); */
+    /*     break; */
+    /*   case 'o': */
+    /*     outputFile = optarg + 1; */
+    /*     printf("outputFile is %s\n", outputFile); */
+    /*     break; */
+    /*   default: */
+    /*     return badInput(); */
+    /*   } */
+    /* } */
+
+    inputFile = argv[1];  /* first argument is input file */
+    outputFile = argv[2]; /* second argument is output file */
+
+    /* fail with wrong usage message if mandatory options missing */
+    if (inputFile == NULL || outputFile == NULL) {
+        return badInput("a");
+    }
+
+    FILE *file = fopen(inputFile, "r");
+    /* check that input file exists and exit if not*/
+    if (file == NULL) {
+        return badInput("n");
+    }
+    /* read bytes to check if JPEG - error out if not */
+    unsigned char bytes[3];
+    fread(bytes, 3, 1, file);
+    if (bytes[0] != 255 || bytes[1] != 216 || bytes[2] != 255) {
+        return badInput("j");
+    }
+
+    /* initialise MagickWand */
+    MagickWand *mw = NULL;
+    mw = NewMagickWand();
+
+    MagickReadImage(mw, inputFile);
+    MagickWriteImage(mw, outputFile);
+
+    printf("Done\n");
+
+    mw = DestroyMagickWand(mw);
+
+    return 0;
+}
+
+/* return if incorrect usage */
+int badInput(char *errorType) {
     switch(*errorType) {
-      case 'a':
-        printf("Not enough arguments \n");
-      break; case 'j' :
-        printf("Input corrupted or not JPG \n");
-      break; case 'n' :
-        printf("Input file nonexistent \n");
-      break; default:
-        printf("Incorrect usage \n");
+        case 'a':
+            printf("Not enough arguments \n");
+        break; case 'j' :
+            printf("Input corrupted or not JPG \n");
+        break; case 'n' :
+            printf("Input file nonexistent \n");
+        break; default:
+            printf("Incorrect usage \n");
     }
     return 1;
-  }
-
-  /* /\* begin implementing options - disabled for now *\/ */
-  /* while ((argIndex = getopt(argc, argv, "i:o")) != -1) { */
-  /*   switch (argIndex) { */
-  /*   case 'i': */
-  /*     inputFile = optarg + 1; */
-  /*     printf("inputFile is %s\n", inputFile); */
-  /*     break; */
-  /*   case 'o': */
-  /*     outputFile = optarg + 1; */
-  /*     printf("outputFile is %s\n", outputFile); */
-  /*     break; */
-  /*   default: */
-  /*     return badInput(); */
-  /*   } */
-  /* } */
-
-  inputFile = argv[1];  /* first argument is input file */
-  outputFile = argv[2]; /* second argument is output file */
-
-  /* fail with wrong usage message if mandatory options missing */
-  if (inputFile == NULL || outputFile == NULL) {
-    return badInput("a");
-  }
-
-  FILE *file = fopen(inputFile, "r");
-  /* check that input file exists and exit if not*/
-  if (file == NULL) {
-    return badInput("n");
-  }
-  /* read bytes to check if JPEG - error out if not */
-  unsigned char bytes[3];
-  fread(bytes, 3, 1, file);
-  if (bytes[0] != 255 || bytes[1] != 216 || bytes[2] != 255) {
-    return badInput("j");
-  }
-
-  /* initialise MagickWand */
-  MagickWand *mw = NULL;
-  mw = NewMagickWand();
-
-  MagickReadImage(mw, inputFile);
-  MagickWriteImage(mw, outputFile);
-
-  printf("Done\n");
-
-  mw = DestroyMagickWand(mw);
-
-  return 0;
 }
