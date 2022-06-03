a620e9b8d0ebbdf688f8a5e983e4484b6a7295a9
felix-u <29074327+felix-u@users.noreply.github.com>
2022-06-03 14:46:06 -0400

    input, output, and verbose arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use clap::{Arg,Command};
+
 fn main() {
-    println!("Hello, world!");
+
+    // parse command-line arguments
+    let args = Command::new("imgclr")
+        .about("Image colouriser")
+        .args(&[
+            Arg::new("input file")
+                .short('i')
+                .long("input")
+                .required(true)
+                .takes_value(true)
+                .help("Supply path to input file"),
+            Arg::new("output file")
+                .short('o')
+                .long("output")
+                .required(true)
+                .takes_value(true)
+                .help("Supply path to output file"),
+            Arg::new("verbose")
+                .short('v')
+                .long("verbose")
+                .required(false)
+                .takes_value(false)
+                .help("Print additional information"),
+        ]).get_matches();
+
+    let input_file = args.value_of("input file").unwrap();
+    let output_file = args.value_of("output file").unwrap();
+
+    if args.is_present("verbose") {
+        print!("Input file: {}\nOutput file: {}\n", input_file, output_file);
+    }
+
 }
