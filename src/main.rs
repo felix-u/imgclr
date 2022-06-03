4999ca33795a8f197d6ac09db7069fe96e1ca01a
felix-u <29074327+felix-u@users.noreply.github.com>
2022-06-03 16:42:37 -0400

    Copy input image to output with no changes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use clap::{Arg,Command};
+use std::path::Path;
+use image::{GenericImageView, ImageBuffer, Pixel};
+use exitcode;
 
 fn main() {
 
@@ -29,8 +32,38 @@ fn main() {
     let input_file = args.value_of("input file").unwrap();
     let output_file = args.value_of("output file").unwrap();
 
+    // check that input file exists and error out if not
+    if !Path::new(input_file).exists() {
+        eprintln!("Error: could not find {}", input_file);
+        std::process::exit(exitcode::NOINPUT);
+    }
+
+    // open the image
+    let img_in = image::open(input_file)
+                        .expect("Could not open image. Caught error");
+    let (width, height) = img_in.dimensions();
+
     if args.is_present("verbose") {
         print!("Input file: {}\nOutput file: {}\n", input_file, output_file);
+        println!("Of dimensions {} by {}", width, height);
+    }
+
+
+    // // create output image
+    // let mut img_out = ImageBuffer::new(width, height);
+    // for (x, y, pixel) in img_in.pixels() {
+    //     // palette conversion process somewhere in here lol
+    //     // FIXME: Don't just copy with no changes lol
+    //     img_out.put_pixel(x, y,
+    //                       pixel.map(|p| p))
+    // }
+
+    // working placeholder: copy input to output path with no changes
+    match img_in.save(output_file) {
+        Ok(()) => {},
+        Err(e) => {
+            eprintln!("Could not save to output path. Caught error:\n{}", e);
+        }
     }
 
 }
