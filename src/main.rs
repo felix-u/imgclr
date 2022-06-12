b8710d4d1ab78818534c3f5e13246e9836309120
felix-u <29074327+felix-u@users.noreply.github.com>
2022-06-11 21:09:39 -0400

    Minor refactoring

--- src/main.rs
+++ src/main.rs
@@ -66,7 +66,7 @@ fn main() -> std::io::Result<()> {
         eprintln!("Error: could not find {}", input_file);
         std::process::exit(exitcode::NOINPUT);
     }
-    
+
     // copy input file to tempfile
     let rand_alphanum: String = thread_rng()
         .sample_iter(&Alphanumeric)
@@ -79,14 +79,14 @@ fn main() -> std::io::Result<()> {
     let temp_file = temp_dir + "/" + rand_alphanum.as_str() + "." + ext;
     println!("Using tempfile at {}", temp_file);
     fs::copy(input_file, &temp_file)?;
-    
+
     // open input image, using tempfile
     let img_in = image::open(&temp_file)
                         .expect("Could not open image. Caught error");
     let (width, height) = img_in.dimensions();
     // open output image
     let mut img_out = RgbImage::new(width, height);
-    
+
 
     // conversion
     for (x, y, pixel) in img_in.clone().pixels() {
@@ -108,7 +108,7 @@ fn main() -> std::io::Result<()> {
             this_g = pixel[1] as i16;
             this_b = pixel[2] as i16;
         }
-    
+
         // find best match
         let mut best_match = 0;
         let mut min_diff: u16 = 999;
@@ -126,33 +126,33 @@ fn main() -> std::io::Result<()> {
         let best_r = clr_match.red as i16;
         let best_g = clr_match.green as i16;
         let best_b = clr_match.blue as i16;
-    
+
         // write pixel
         img_out.put_pixel(x, y, Rgb([best_r as u8, best_g as u8, best_b as u8]));
-        
+
         // dithering - see https://en.wikipedia.org/wiki/Floyd-Steinberg_dithering
         if !args.is_present("disable dithering") {
-            
+
             let quant_error: [i16; 3] = [
                 this_r - best_r,
                 this_g - best_g,
                 this_b - best_b
             ];
-            
+
             // operates on the following, where * is the current pixel
             //   * 1
             // 2 3 4
-            
+
             // 1
             if x < (width - 1) {
                 let that_pix = img_in.get_pixel(x+1, y);
                 let that_r = that_pix[0];
                 let that_g = that_pix[1];
                 let that_b = that_pix[2];
-                put_quantised(x+1, y, quant_error, 7, 
+                put_quantised(x+1, y, quant_error, 7,
                     [that_r, that_g, that_b], img_in.clone());
             }
-            
+
             // 2
             if x > 0 && y < (height - 1) {
                 let that_pix = img_in.get_pixel(x-1, y+1);
@@ -162,7 +162,7 @@ fn main() -> std::io::Result<()> {
                 put_quantised(x-1, y+1, quant_error, 3,
                     [that_r, that_g, that_b], img_in.clone());
             }
-            
+
             // 3
             if y < (height - 1) {
                 let that_pix = img_in.get_pixel(x, y+1);
@@ -172,7 +172,7 @@ fn main() -> std::io::Result<()> {
                 put_quantised(x, y+1, quant_error, 5,
                     [that_r, that_g, that_b], img_in.clone());
             }
-            
+
             // 4
             if x < (width - 1) && y < (height - 1) {
                 let that_pix = img_in.get_pixel(x+1, y+1);
@@ -185,7 +185,7 @@ fn main() -> std::io::Result<()> {
 
         }
     }
-    
+
     // save image to output path
     match img_out.save(output_file) {
         Ok(()) => {},
@@ -193,7 +193,7 @@ fn main() -> std::io::Result<()> {
             eprintln!("Couldn't save output. Caught error: {}", e);
         }
     }
-       
+
     // FIXME: uncomment when done implementing dithering!
     // fs::remove_file(temp_file)?;
     println!("Wrote image of size {}x{} to {}", width, height, output_file);
@@ -203,11 +203,11 @@ fn main() -> std::io::Result<()> {
 
 fn put_quantised(loc_x: u32, loc_y: u32, error: [i16; 3], numerator: i16,
                              channels: [u8; 3], mut some_img: DynamicImage) {
-    
+
     let new_r = (channels[0] as i16 + error[0] * numerator / 16) as u8;
     let new_g = (channels[1] as i16 + error[1] * numerator / 16) as u8;
     let new_b = (channels[2] as i16 + error[2] * numerator / 16) as u8;
     some_img.put_pixel(loc_x, loc_y, Rgba([
-        new_r, new_g, new_b, 255  
+        new_r, new_g, new_b, 255
     ]))
 }
