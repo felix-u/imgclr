8fa415ba1b12717b7029db94c66074eea72908b1
felix-u <29074327+felix-u@users.noreply.github.com>
2022-07-10 13:37:41 -0400

    Still not functional

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ use std::path::Path;
 use rand::{thread_rng, Rng};
 use rand::distributions::Alphanumeric;
 
-
 fn main() -> std::io::Result<()> {
 
     // parse command-line arguments
@@ -89,100 +88,104 @@ fn main() -> std::io::Result<()> {
 
 
     // conversion
-    for (x, y, pixel) in img_in.clone().pixels() {
+    // for (x, y, pixel) in img_in.clone().pixels() {
+    for x in 0..height {
+        for y in 0..width {
 
-        // get current pixel
-        let this_r: i16;
-        let this_g: i16;
-        let this_b: i16;
-        if args.is_present("swap luma")  {
-            let this_pix = ClrpColor::new_rgb(pixel[0], pixel[1], pixel[2])
-                            .invert_luminescence();
-            this_r = this_pix.red as i16;
-            this_g = this_pix.green as i16;
-            this_b = this_pix.blue as i16;
-        }
-        else {
-            // pixel is an array. index 0 is R, 1 is G, 2 is B, and 3 is alpha
-            this_r = pixel[0] as i16;
-            this_g = pixel[1] as i16;
-            this_b = pixel[2] as i16;
-        }
-
-        // find best match
-        let mut best_match = 0;
-        let mut min_diff: u16 = 999;
-        for i in 0..Vec::len(&palette) {
-            let comp_r: u16 = this_r.abs_diff(palette[i].red as i16).into();
-            let comp_g: u16 = this_g.abs_diff(palette[i].green as i16).into();
-            let comp_b: u16 = this_b.abs_diff(palette[i].blue as i16).into();
-            let diff_total: u16 = comp_r + comp_g + comp_b;
-            if diff_total < min_diff {
-                min_diff = diff_total;
-                best_match = i;
+            // get current pixel
+            let this_r: i16;
+            let this_g: i16;
+            let this_b: i16;
+            let pixel = img_in.get_pixel(x, y);
+            if args.is_present("swap luma")  {
+                let this_pix = ClrpColor::new_rgb(pixel[0], pixel[1], pixel[2])
+                                .invert_luminescence();
+                this_r = this_pix.red as i16;
+                this_g = this_pix.green as i16;
+                this_b = this_pix.blue as i16;
             }
-        }
-        let clr_match = &palette[best_match];
-        let best_r = clr_match.red as i16;
-        let best_g = clr_match.green as i16;
-        let best_b = clr_match.blue as i16;
-
-        // write pixel
-        img_out.put_pixel(x, y, Rgb([best_r as u8, best_g as u8, best_b as u8]));
-
-        // dithering - see https://en.wikipedia.org/wiki/Floyd-Steinberg_dithering
-        if !args.is_present("disable dithering") {
-
-            let quant_error: [i16; 3] = [
-                this_r - best_r,
-                this_g - best_g,
-                this_b - best_b
-            ];
-
-            // operates on the following, where * is the current pixel
-            //   * 1
-            // 2 3 4
-
-            // 1
-            if x < (width - 1) {
-                let that_pix = img_in.get_pixel(x+1, y);
-                let that_r = that_pix[0];
-                let that_g = that_pix[1];
-                let that_b = that_pix[2];
-                put_quantised(x+1, y, quant_error, 7,
-                    [that_r, that_g, that_b], img_in.clone());
+            else {
+                // pixel is an array. index 0 is R, 1 is G, 2 is B, and 3 is alpha
+                this_r = pixel[0] as i16;
+                this_g = pixel[1] as i16;
+                this_b = pixel[2] as i16;
             }
 
-            // 2
-            if x > 0 && y < (height - 1) {
-                let that_pix = img_in.get_pixel(x-1, y+1);
-                let that_r = that_pix[0];
-                let that_g = that_pix[1];
-                let that_b = that_pix[2];
-                put_quantised(x-1, y+1, quant_error, 3,
-                    [that_r, that_g, that_b], img_in.clone());
+            // find best match
+            let mut best_match = 0;
+            let mut min_diff: u16 = 999;
+            for i in 0..Vec::len(&palette) {
+                let comp_r: u16 = this_r.abs_diff(palette[i].red as i16).into();
+                let comp_g: u16 = this_g.abs_diff(palette[i].green as i16).into();
+                let comp_b: u16 = this_b.abs_diff(palette[i].blue as i16).into();
+                let diff_total: u16 = comp_r + comp_g + comp_b;
+                if diff_total < min_diff {
+                    min_diff = diff_total;
+                    best_match = i;
+                }
             }
+            let clr_match = &palette[best_match];
+            let best_r = clr_match.red as i16;
+            let best_g = clr_match.green as i16;
+            let best_b = clr_match.blue as i16;
 
-            // 3
-            if y < (height - 1) {
-                let that_pix = img_in.get_pixel(x, y+1);
-                let that_r = that_pix[0];
-                let that_g = that_pix[1];
-                let that_b = that_pix[2];
-                put_quantised(x, y+1, quant_error, 5,
-                    [that_r, that_g, that_b], img_in.clone());
+            // write pixel
+            img_out.put_pixel(x, y, Rgb([best_r as u8, best_g as u8, best_b as u8]));
+
+            // dithering - see https://en.wikipedia.org/wiki/Floyd-Steinberg_dithering
+            if !args.is_present("disable dithering") {
+
+                let quant_error: [i16; 3] = [
+                    this_r - best_r,
+                    this_g - best_g,
+                    this_b - best_b
+                ];
+
+                // operates on the following, where * is the current pixel
+                //   * 1
+                // 2 3 4
+
+                // 1
+                if x < (width - 1) {
+                    let that_pix = img_in.get_pixel(x+1, y);
+                    let that_r = that_pix[0];
+                    let that_g = that_pix[1];
+                    let that_b = that_pix[2];
+                    put_quantised(x+1, y, quant_error, 7,
+                        [that_r, that_g, that_b], &mut img_in.clone());
+                }
+
+                // 2
+                if x > 0 && y < (height - 1) {
+                    let that_pix = img_in.get_pixel(x-1, y+1);
+                    let that_r = that_pix[0];
+                    let that_g = that_pix[1];
+                    let that_b = that_pix[2];
+                    put_quantised(x-1, y+1, quant_error, 3,
+                        [that_r, that_g, that_b], &mut img_in.clone());
+                }
+
+                // 3
+                if y < (height - 1) {
+                    let that_pix = img_in.get_pixel(x, y+1);
+                    let that_r = that_pix[0];
+                    let that_g = that_pix[1];
+                    let that_b = that_pix[2];
+                    put_quantised(x, y+1, quant_error, 5,
+                        [that_r, that_g, that_b], &mut img_in.clone());
+                }
+
+                // 4
+                if x < (width - 1) && y < (height - 1) {
+                    let that_pix = img_in.get_pixel(x+1, y+1);
+                    let that_r = that_pix[0];
+                    let that_g = that_pix[1];
+                    let that_b = that_pix[2];
+                    put_quantised(x+1, y+1, quant_error, 1,
+                        [that_r, that_g, that_b], &mut img_in.clone());
+                }
+
             }
-
-            // 4
-            if x < (width - 1) && y < (height - 1) {
-                let that_pix = img_in.get_pixel(x+1, y+1);
-                let that_r = that_pix[0];
-                let that_g = that_pix[1];
-                let that_b = that_pix[2];
-                put_quantised(x+1, y+1, quant_error, 1,
-                    [that_r, that_g, that_b], img_in.clone());
-            }
-
         }
     }
 
@@ -202,8 +205,7 @@ fn main() -> std::io::Result<()> {
 
 
 fn put_quantised(loc_x: u32, loc_y: u32, error: [i16; 3], numerator: i16,
-                             channels: [u8; 3], mut some_img: DynamicImage) {
-
+                             channels: [u8; 3], some_img: &mut DynamicImage) {
     let new_r = (channels[0] as i16 + error[0] * numerator / 16) as u8;
     let new_g = (channels[1] as i16 + error[1] * numerator / 16) as u8;
     let new_b = (channels[2] as i16 + error[2] * numerator / 16) as u8;
