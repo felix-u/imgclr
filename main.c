4684be144045c774f048ce5b90b73c8af07b2ea6
felix-u <29074327+felix-u@users.noreply.github.com>
2022-07-10 21:46:45 -0400

    Add stb_image libraries (unused)

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <getopt.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 
 // EDIT PALETTE HERE
 // default is solarised
