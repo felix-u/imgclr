c16ab4a3795e99eb849741bf771e0fc7b95e104b
felix-u <29074327+felix-u@users.noreply.github.com>
2022-08-22 21:32:51 -0400

    Initial file for parsing args

--- src/main.c
+++ src/main.c
@@ -1,9 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sysexits.h>
+#include <stdint.h>
 
-int main() {
+
+int main(int argc, char** argv) {
     printf("Hej!\n");
+
+    for (int i = 0; i < argc; i++) {
+        printf("%s\n", argv[i]);
+    }
 
     return EXIT_SUCCESS;
 }
