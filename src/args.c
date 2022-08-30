e097df024a7ad5e0e15f6b37a39c603a02c2c254
felix-u <29074327+felix-u@users.noreply.github.com>
2022-08-29 23:44:41 -0400

    Check for all arguments

--- src/args.c
+++ src/args.c
@@ -13,10 +13,14 @@ typedef struct BoolFlagReturn {
 
 // Checks if a boolean flag exists
 
-BoolFlagReturn args_isPresent(int argc, char** argv, char* flag) {
+BoolFlagReturn args_isPresent(int argc, char** argv, char** flag) {
 
-    for (int i = 0; i < argc; i++) {
-        if (strcasecmp(argv[i], flag)) {
+    for (int i = 1; i < argc; i++) {
+        if (!strcasecmp(argv[i], flag[0])) {
+            BoolFlagReturn this_return = {true, i};
+            return this_return;
+        }
+        else if (!strcasecmp(argv[i], flag[1])) {
             BoolFlagReturn this_return = {true, i};
             return this_return;
         }
@@ -30,7 +34,7 @@ BoolFlagReturn args_isPresent(int argc, char** argv, char* flag) {
 
 // Returns single value of flag
 
-char* args_singleValueOf(int argc, char** argv, char* flag) {
+char* args_singleValueOf(int argc, char** argv, char** flag) {
 
     // Check the flag has actually been passed
     BoolFlagReturn flag_check = args_isPresent(argc, argv, flag);
@@ -38,16 +42,16 @@ char* args_singleValueOf(int argc, char** argv, char* flag) {
 
         // If the arg after the flag doesn't start with '-', we return it as
         // the supplied option.
-        if (argc > flag_check.index && argv[flag_check.index][0] != '-') {
-            return argv[flag_check.index];
+        if (argc > flag_check.index + 1 && argv[flag_check.index + 1][0] != '-') {
+            return argv[flag_check.index + 1];
         }
 
         // Otherwise, flag is present but no value supplied
-        return "";
+        return NULL;
     }
 
     // Flag not present
-    return "";
+    return NULL;
 }
 
 
@@ -55,39 +59,41 @@ char* args_singleValueOf(int argc, char** argv, char* flag) {
 
 typedef struct MultipleValReturn {
     int offset;
-    int length;
+    int end;
 } MultipleValReturn;
 
 
 // Returns multiple values of flag
 
-MultipleValReturn args_multipleValuesOf(int argc, char** argv, char* flag) {
+MultipleValReturn args_multipleValuesOf(int argc, char** argv, char** flag) {
 
     // Check the flag has actually been passed
     BoolFlagReturn flag_check = args_isPresent(argc, argv, flag);
+
     if (flag_check.is_present) {
 
         // If the arg after the flag doesn't start with '-', at least one
         // option was supplied and so there are things to return.
-        if (argc > flag_check.index && argv[flag_check.index][0] != '-') {
+        if (argc > flag_check.index + 1 && argv[flag_check.index + 1][0] != '-') {
+
             int end_index = 0;
 
             // Stop at first argument which begins with '-'
-            for (int i = flag_check.index; i < argc; i++) {
+            for (int i = flag_check.index + 1; i < argc; i++) {
                 if (argv[i][0] == '-') {
                     end_index = i;
                 }
             }
 
-            // Or, if end_index is still 0, the values go to the end of argv
+            // If end_index is still 0 by now, the values go to the end of argv.
             if (end_index == 0) end_index = argc;
 
-            MultipleValReturn this_return = {flag_check.index, end_index};
+            MultipleValReturn this_return = {flag_check.index + 1, end_index};
             return this_return;
         }
 
         // Otherwise, flag is present but no value supplied
-        MultipleValReturn this_return = {flag_check.index, 0};
+        MultipleValReturn this_return = {flag_check.index + 1, 0};
         return this_return;
     }
 
--- src/main.c
+++ src/main.c
@@ -4,22 +4,78 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdbool.h>
+
 #include "args.c"
+
 #define STB_DS_IMPLEMENTATION
 #include "../libs/stb_ds-v0.67/stb_ds.h"
 
 
-int main(int argc, char** argv) {
+const char *HELP_TEXT =
+"    -h, --help                Display this help information and exit.\n"
+"    -i, --input <FILE>        Supply input file.\n"
+"    -o, --output <FILE>       Supply output location.\n"
+"    -d, --dither <STR>        Specify dithering algorithm, or \"none\" to disable\n"
+"                                  (default is \"floyd-steinberg\").\n"
+"    -s, --swap                Invert image brightness, preserving hue and\n"
+"                              saturation.\n"
+"    -p, --palette <STR>...    Supply palette as whitespace-separated colours.\n";
 
-    char *input_arg;
-    if (args_isPresent(argc, argv, "-i").is_present == true) {
-        input_arg = "-i";
+
+int main(int argc, char **argv) {
+
+    // Help flag
+
+    char **help_arg = (char *[]){"-h", "--help"};
+    BoolFlagReturn help = args_isPresent(argc, argv, help_arg);
+    if (help.is_present) {
+        printf("%s\n", HELP_TEXT);
+        return(EXIT_SUCCESS);
     }
-    else {
-        printf("ERROR: Provide path to input file\n");
+
+
+    // Required arguments
+
+    char **input_arg = (char *[]){"-i", "--input"};
+    char *input_path = args_singleValueOf(argc, argv, input_arg);
+    if (input_path == NULL) {
+        printf("ERROR: Must provide path to input file.\n");
         exit(EX_USAGE);
     }
-    printf("%s\n", input_arg);
+
+    char **output_arg = (char *[]){"-o", "--output"};
+    char *output_path = args_singleValueOf(argc, argv, output_arg);
+    if (output_path == NULL) {
+        printf("ERROR: Must provide output location.\n");
+        exit(EX_USAGE);
+    }
+
+    char **palette_arg = (char *[]){"-p", "--palette"};
+    MultipleValReturn palette_return =
+        args_multipleValuesOf(argc, argv, palette_arg);
+    if (palette_return.offset == 0 || palette_return.end == 0 ||
+        palette_return.end - palette_return.offset < 2)
+    {
+        printf("ERROR: Must provide at least two palette colours.\n");
+        exit(EX_USAGE);
+    }
+
+
+    // Optional arguments
+
+    char **dither_arg = (char *[]){"-d", "--dither"};
+    BoolFlagReturn dither_is_present = args_isPresent(argc, argv, dither_arg);
+    if (dither_is_present.is_present) {
+        char *dither_alg = args_singleValueOf(argc, argv, dither_arg);
+        if (dither_alg == NULL) {
+            printf("ERROR: `dither` flag requires argument.\n");
+            exit(EX_USAGE);
+        }
+    }
+
+    char **swap_arg = (char *[]){"-s", "--swap"};
+    BoolFlagReturn swap = args_isPresent(argc, argv, swap_arg);
+
 
     return EXIT_SUCCESS;
 }
