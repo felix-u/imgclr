9e9822420910118d7919ab96f14fa8b150c6f03c
felix-u <29074327+felix-u@users.noreply.github.com>
2023-02-09 21:23:08 -0500

    Comments

--- src/main.c
+++ src/main.c
@@ -184,34 +184,8 @@ int main(int argc, char **argv) {
         }
     }
 
-    // NOTE: I no longer think this is worth the effort, since a few int
-    // comparisons per pixel don't seem necessarily more expensive than a few
-    // additions and divisions to calculate the target index from an x and y
-    // coordinate. It might even be cheaper the way it's done now. But I'm
-    // leaving this here in case I want to come back to it.
-
-    // // Establish bounds for edge-case control loops, when not all error can be
-    // // diffused because some of the neighbouring pixels targeted by the
-    // // dithering algorithm are out of bounds.
-    //
-    // int x_bound_left = 0;
-    // int x_bound_right = width;
-    // int y_bound_bottom = height;
-    //
-    // for (int i = 0; i < algorithm->offset_num; i++) {
-    //     if (x_bound_left + algorithm->offsets[i].x < 0) {
-    //         x_bound_left = 0 - algorithm->offsets[i].x;
-    //     }
-    //     if (width - algorithm->offsets[i].x < x_bound_right) {
-    //         x_bound_right = width - algorithm->offsets[i].x;
-    //     }
-    //     if (height - algorithm->offsets[i].y < y_bound_bottom) {
-    //         y_bound_bottom = height - algorithm->offsets[i].y;
-    //     }
-    // }
-
-
-    // Convert image to palette
+    // NOTE: Having several loops to avoid bounds checking on the majority of the image may seem appealing. It is NOT
+    // worth it.
 
     for (usize i = 0; i < data_len; i += 3) {
 
@@ -250,10 +224,6 @@ int main(int argc, char **argv) {
             isize target_x = current_x + algorithm->offsets[j].x;
             isize target_y = current_y + algorithm->offsets[j].y;
 
-            // @Speed { We have the information to avoid doing bounds checking
-            // on the majority of the image, so this is low-hanging fruit. But
-            // this is probably not worth it (see NOTE above). }
-
             if (target_x < 0 || target_x >= width || target_y < 0 || target_y >= height) continue;
 
             usize target_index = channels * (target_y * width + target_x);
@@ -279,7 +249,6 @@ int main(int argc, char **argv) {
         }
 
     }
-
 
     // Write to output path, with correct format based on extension
 
