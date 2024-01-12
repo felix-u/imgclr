4cb8feb2b9c1f4a45228a6f02d0ffff665c91f9d
Felix <29074327+felix-u@users.noreply.github.com>
2024-01-11 21:55:29 -0500

    hello world

--- imgclr.asm
+++ imgclr.asm
@@ -1,8 +1,25 @@
 format PE64 console
 entry start
 
-section '.text' code readable executable
+include 'include/win64ax.inc'
 
+section '.data' data readable
+msg db 'Hello, world!',0
+msg_len equ $ - msg
+
+section '.text' code readable executable
 start:
+    cinvoke printf, "Hello, world!%c",10
+
     mov rax, 0
     ret
+
+section '.idata' import data readable writeable
+
+library \
+    kernel32, 'kernel32.dll', \
+    msvcrt, 'msvcrt.dll'
+
+import msvcrt, \
+    printf, 'printf'
+
