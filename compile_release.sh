74bd5adb948b9bedda87ea3fd0c6b365cc62fd08
felix-u <29074327+felix-u@users.noreply.github.com>
2024-01-20 17:38:03 -0500

    convert compile_release script to POSIX sh

---
+++ compile_release.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+version=0.2
+cc="zig cc -O3 -w -static -s src/main.c"
+
+mkdir -p release/
+
+$cc -target x86_64-windows     -o release/imgclr-v"$version"-x86_64-win.exe 
+$cc -target aarch64-windows    -o release/imgclr-v"$version"-aarch64-win.exe
+$cc -target x86_64-linux-musl  -o release/imgclr-v"$version"-x86_64-linux   
+$cc -target aarch64-linux-musl -o release/imgclr-v"$version"-aarch64-linux  
+$cc -target x86_64-macos       -o release/imgclr-v"$version"-x86_64-macos   
+$cc -target aarch64-macos      -o release/imgclr-v"$version"-aarch64-macos  
